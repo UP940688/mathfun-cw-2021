6fe38c611681c19e97a79c9566d751ce7a675b12
Luke Trimmer <up940688@myport.ac.uk>
2021-04-20 16:33:55 +0100

    handle input loc differently

--- 940688.hs
+++ 940688.hs
@@ -79,16 +79,14 @@ getNames = map getName
 getPopulation :: [City] -> Name -> Index -> FormattedPopulation
 getPopulation = fmap populationAt . cityFromName
 
-maybeRecord :: City -> Index -> Maybe Population
-maybeRecord (City _ _ records) i
+maybeRecord :: Index -> City -> Maybe Population
+maybeRecord i (City _ _ records)
   | i >= 0 && i < length records = Just (records !! i)
   | otherwise = Nothing
 
--- maybe takes default b, function (a -> b), and (Maybe a)
--- if Nothing, return default, if (Just a), return result of (a -> b)
+-- f =<< Nothing = Nothing, f =<< (Just a) =  f
 populationAt :: Maybe City -> Index -> FormattedPopulation
-populationAt (Just city) i = maybe "no data" fmtPopulation (maybeRecord city i)
-populationAt Nothing _ = "no data"
+populationAt city idx = maybe "no data" fmtPopulation (maybeRecord idx =<< city)
 
 --------------------------------------------------
 --                  section three               --
@@ -218,7 +216,6 @@ adjustCursor ys = goTo (0, maximum ys + 4) -- 4 below lowest mapped city
 drawCity :: City -> IO Int
 drawCity (City name loc records) = do
   writeAt (x, y) ("+ " ++ name)
-  -- x $ f a b is equivalent to x (f a b)
   writeAt (x, y + 1) $ (fmtPopulation . head) records
   return y
   where (x, y) = locationToPosition loc
@@ -238,8 +235,8 @@ main = do
       (len1, len2) = (length tmp, length cities)
       colour = if len1 - len2 == 0 then green else red
   -- notify the user how many have been filtered out
-  printf "\nINFO: Loaded %s cities.\n" $ colour (printf "%i/%i" len2 len1)
-  putStrLn ('\n' : getPrettyNamesString cities)
+  printf "\nINFO: Loaded %s cities.\n\n" $ colour (printf "%i/%i" len2 len1)
+  putStrLn (getPrettyNamesString cities)
   updatedCities <- loopChoices cities
   writeFile "cities.txt" updatedCities
 
@@ -248,7 +245,7 @@ fileToCities :: FilePath -> IO [Maybe City]
 fileToCities fp = map readMaybe . lines <$> readFile fp
 
 showOptions :: IO ()
-showOptions = putStr $ underline "Options:" ++
+showOptions = putStr $ underline "\nOptions:" ++
   "\n\n\
   \(1) Show names \n\
   \(2) Return population of city n years ago \n\
@@ -286,11 +283,11 @@ loopChoices cities = do
   updatedCities <- case choice of
     "4" -> do
       new <- fmap (updateRecords cities) getListOfIntsIO
-      putStrLn $ citiesToString new
+      putStr $ citiesToString new
       return new
     "5" -> do
       new <- doinsertIO cities
-      putStrLn $ citiesToString new
+      putStr $ citiesToString new
       return new
     _ -> matchChoice cities choice >> return cities
 
@@ -302,16 +299,16 @@ matchChoice :: [City] -> String -> IO ()
 matchChoice cities choice
   | choice == "1" = putStrLn $ getPrettyNamesString cities
   | choice == "2" = doPopulationIO cities
-  | choice == "3" = putStrLn $ citiesToString cities
+  | choice == "3" = putStr $ citiesToString cities
   | choice == "6" = doAnnualGrowthIO cities
   | choice == "7" = doFindCityIO cities
   | choice == "8" = drawCities cities
   | choice `elem` ["4", "5", "9"] = return ()
-  | otherwise = putStrLn (red "Invalid choice.\n")
+  | otherwise = putStrLn (red "Invalid choice.")
 
 getPrettyNamesString :: [City] -> String
-getPrettyNamesString = (underline "City Names:\n\n" ++) . nameList
-  where nameList = concatMap (\ (City name _ _) -> printf "• %s\n" name)
+getPrettyNamesString = (underline "City Names:\n" ++) . nameList
+  where nameList = concatMap (\ (City name _ _) -> printf "\n• %s" name)
 
 getCityNameIO :: IO Name
 getCityNameIO = putStr "Please enter city name:" >> promptUser
@@ -319,48 +316,47 @@ getCityNameIO = putStr "Please enter city name:" >> promptUser
 getIntIO :: String -> IO (Maybe Int)
 getIntIO str = putStr str >> readMaybe <$> promptUser
 
+getLocationIO :: String -> IO (Maybe (Int, Int))
+getLocationIO str = putStr str >> readMaybe <$> promptUser
+
 getListOfIntsIO :: IO [Int]
 getListOfIntsIO = do
   input <- getIntIO "Please enter an integer (non-integer to finish)"
-  maybe (return []) (\ i -> (i :) <$> getListOfIntsIO) input  
+  maybe (return []) (\ i -> (i :) <$> getListOfIntsIO) input
 
 doFindCityIO :: [City] -> IO ()
 doFindCityIO cities = do
-  north <- getIntIO "Please enter city's location (degrees north):"
-  east <- getIntIO "Please enter city's location (degrees east):"
+  loc <- getLocationIO "Please enter city's location in form (N, E):"
   pop <- getIntIO "Please enter minimum population city should have:"
-  case (north, east, pop) of
-    (Just n, Just e, Just p) ->
-      printf "Nearest City: %s\n\n" (nearestCityName cities (Loc n e) p)
-    _ -> putStrLn $ red "Invalid population figure entered."
+  putStrLn $ case (loc, pop) of
+    (Just (n, e), Just p) -> "Nearest City: " ++ nearestCityName cities (Loc n e) p
+    _ -> red "Invalid population figure entered."
 
 doPopulationIO :: [City] -> IO ()
 doPopulationIO cities = do
-  city <- getCityNameIO
-  idx <- getIntIO "Please enter how many years ago to get records (0 for current):"
-  putStrLn $ maybe (red "Please enter valid integer.") (population city) idx
+  name <- getCityNameIO
+  idx <- getIntIO "Please enter how many years ago to get records (0 = current):"
+  putStrLn $ maybe (red "Please enter valid integer.") (populationOf name) idx
   where
-    population name = printf "Population: %s\n\n" . getPopulation cities name
+    populationOf name = printf "Population: %s" . getPopulation cities name
 
 doAnnualGrowthIO :: [City] -> IO ()
 doAnnualGrowthIO cities = do
   name <- getCityNameIO
-  let growths = unlines $ printf "• %.2f%%" <$> cityGrowth cities name
-  if null growths
-    then putStrLn "No data available.\n"
-    else putStrLn $ underline "Annual Growth Figures:\n\n" ++ growths
+  let growths = concat $ printf "\n• %.2f%%" <$> cityGrowth cities name
+  putStrLn $ if null growths
+    then "No data available."
+    else underline "Annual Growth Figures:\n" ++ growths
 
 doinsertIO :: [City] -> IO [City]
 doinsertIO cities = do
   name <- getCityNameIO
-  north <- getIntIO "Please enter degrees north:"
-  east <- getIntIO "Please enter degrees east:"
+  maybeLoc <- getLocationIO "Please enter city's location in form (N, E):"
   putStrLn "Please enter population figures (from most recent, 2+ entries)"
-  records <- getListOfIntsIO
-  case (north, east) of
-    (Just n, Just e)
-      | length records >= 2 -> return (cities `insert` City name (Loc n e) records)
-      | otherwise -> do
-          putStrLn $ red "Not enough population figures, not adding city.\n"
-          return cities
-    _ -> putStrLn (red "Invalid data, not adding city.\n") >> return cities
+  pops <- getListOfIntsIO
+  maybe (err "Invalid data, not adding city." cities) (\ (n, e) ->
+      if length pops >= 2
+        then return (cities `insert` City name (Loc n e) pops)
+        else err "Not enough population figures, not adding city." cities
+    ) maybeLoc
+  where err str cities = putStrLn (red str) >> return cities
